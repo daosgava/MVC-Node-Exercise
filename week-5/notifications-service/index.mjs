42f647c4d53ff33422db8a78d1017f575e04fefa
daosgava <daosgava.garcia@gmail.com>
2024-12-06 15:05:05 +1100

    add CORS support to notifications service

--- week-5/notifications-service/index.mjs
+++ week-5/notifications-service/index.mjs
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors"; 
 import notificationsRouter from "./routes/notifications.mjs";
 const app = express();
 const port = 3000;
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+const corsOptions = {
+  origin: "http://localhost:3003",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+app.use(cors(corsOptions));
 
 app.use('/api/notifications', notificationsRouter);
 

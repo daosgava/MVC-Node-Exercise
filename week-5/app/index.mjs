295ccaf0c4b0364f6c7531aab152ec8e36ec2c30
daosgava <daosgava.garcia@gmail.com>
2025-01-05 13:15:31 +1100

    Add MVC to fronend

--- week-5/app/index.mjs
+++ week-5/app/index.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import ejs from "ejs";
+import appRouter from "./routes/app.mjs";
 const app = express();
 const port = 3003;
 
@@ -8,9 +9,7 @@ app.set("view engine", "html");
 app.set("views", "./views");
 app.use(express.static("public"));
 
-app.get("/", (_, res) => {
-  res.render("index.html", { title: "Welcome Page" });
-});
+app.use("/", appRouter);
 
 app.listen(port, () => {
 	console.log(`App listening at http://localhost:${port}`);
